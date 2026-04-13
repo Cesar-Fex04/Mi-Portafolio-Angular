d29af362f878f46d361d7977cdce87c595df1c9d
Cesar-Fex04 <a222200714@unison.mx>
2026-04-12 21:40:01 -0700

    first commit

---
+++ src/app/servicios/servicios.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servicios',
+  standalone: true,
+  imports: [],
+  templateUrl: './servicios.html',
+  styleUrl: './servicios.css'
+})
+export class ServiciosComponent {}
